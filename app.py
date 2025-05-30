009016ae9f37f44f9ede0debef48822685b760b4
Igor <igor.lisboa1967@gmail.com>
2025-05-30 11:35:09 -0300

    Desenvolvimento

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return '🚀 Aplicação Flask funcionando no Render!'
+
+@app.route('/html')
+def pagina_html():
+    return render_template('pagina.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
